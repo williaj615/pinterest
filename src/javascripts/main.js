7bfca8e4aa4addf7e61abd73cb4682b4667e034b
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-11-09 10:18:57 -0600

    initialized firebase app

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,11 @@
-import 'bootstrap';
+import firebase from 'firebase';
+
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+};
+
+init();
